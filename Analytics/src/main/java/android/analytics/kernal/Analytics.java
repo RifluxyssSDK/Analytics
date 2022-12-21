56bd2b0d4f13fe7a2583d1e3c3c74f2124a3e2e8
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-12-21 12:09:20 +0530

    Analytics - init commit

--- Analytics/src/main/java/android/analytics/kernal/Analytics.java
+++ Analytics/src/main/java/android/analytics/kernal/Analytics.java
@@ -4,7 +4,6 @@ import android.analytics.Utils;
 import android.content.Context;
 
 public class Analytics extends Utils {
-
     public static void init(Context context) {
         getUtils().setContext(context);
     }
